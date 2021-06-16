e0701c8260d0ff7a200987817a86cf0fbe4a010b
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-16 10:39:31 -0400

    commented out socket

--- controllers/restaurant-ctrl.js
+++ controllers/restaurant-ctrl.js
@@ -19,7 +19,7 @@ createRestaurant = (req, res) => {
     restaurant
         .save()
         .then(() => {
-            //socket.emit("product-created", restaurant)
+            socket.emit("product-created", restaurant)
             return res.status(201).json({
                 success: true,
                 id: restaurant._id,
@@ -86,7 +86,7 @@ deleteRestaurant = async (req, res) => {
                 .status(404)
                 .json({ success: false, error: `Restaurant not found` })
         }
-        socket.emit("product-deleted", restaurant)
+        //socket.emit("product-deleted", restaurant)
         return res.status(200).json({ success: true, data: restaurant })
     }).catch(err => console.log(err))
 }
